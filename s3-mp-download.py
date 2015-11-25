86e83062d51483b21b2011587ee7c4f126560e20
Zifei Shan <zifeishan@gmail.com>
2015-11-25 13:21:00 -0800

    Fixed a bug in retry downloading
    There seems to be a bug when retrying download. Currently it tries to call do_part_download and get following error:
    
    ```
    ERROR:s3-mp-download:do_part_download() takes exactly 1 argument (9 given)
    ```

--- s3-mp-download.py
+++ s3-mp-download.py
@@ -79,7 +79,7 @@ def do_part_download(args):
         else:
             time.sleep(3)
             current_tries += 1
-            do_part_download(bucket_name, key_name, fname, min_byte, max_byte, split, secure, max_tries, current_tries)
+            do_part_download((bucket_name, key_name, fname, min_byte, max_byte, split, secure, max_tries, current_tries))
 
 def gen_byte_ranges(size, num_parts):
     part_size = int(ceil(1. * size / num_parts))
